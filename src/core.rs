efbb7ec201d43e2e388b77e06b0723662fb59842
Martijn Faassen <faassen@startifact.com>
2024-08-03 17:08:32 +0200

    New approach to decoding using a trait.

--- src/core.rs
+++ src/core.rs
@@ -1,134 +1,237 @@
-#[derive(Debug, PartialEq, Eq)]
-enum OpCode {
-    Special0 = 0,
-    Special1,
-    Store,
-    Load,
-    Add,
-    Sub,
-    Mul,
-    Div,
-    Eq,
-    Gt,
-    And,
-    Or,
-    Xor,
-    Unknown0,
-    Unknown1,
-    Pattern,
+// the executor trait can execute actual instructions
+trait Executor {
+    fn call(&mut self);
+    fn return_(&mut self);
+    fn value(&mut self); // this instruction actually would need to take a register too
+    fn if_(&mut self, r0: u8);
+    fn repeat(&mut self, r0: u8);
+    fn not(&mut self, r0: u8);
+    fn push(&mut self, r0: u8);
+    fn pop(&mut self, r0: u8);
+    fn inc(&mut self, r0: u8);
+    fn dec(&mut self, r0: u8);
+    fn store(&mut self, r0: u8, r1: u8);
+    fn load(&mut self, r0: u8, r1: u8);
+    fn add(&mut self, r0: u8, r1: u8);
+    fn sub(&mut self, r0: u8, r1: u8);
+    fn mul(&mut self, r0: u8, r1: u8);
+    fn div(&mut self, r0: u8, r1: u8);
+    fn eq(&mut self, r0: u8, r1: u8);
+    fn gt(&mut self, r0: u8, r1: u8);
+    fn and(&mut self, r0: u8, r1: u8);
+    fn or(&mut self, r0: u8, r1: u8);
+    fn xor(&mut self, r0: u8, r1: u8);
+    fn unknown0(&mut self, r0: u8, r1: u8);
+    fn unknown1(&mut self, r0: u8, r1: u8);
 }
 
-#[derive(Debug, PartialEq, Eq)]
-enum Special0 {
-    Block = 0,
-    Call,
-    Return,
-    Value,
-    IfR0,
-    IfR1,
-    IfR2,
-    IfR3,
-    RepeatR0,
-    RepeatR1,
-    RepeatR2,
-    RepeatR3,
-    NotR0,
-    NotR1,
-    NotR2,
-    NotR3,
-}
-
-#[derive(Debug, PartialEq, Eq)]
-enum Special1 {
-    PushR0 = 0,
-    PushR1,
-    PushR2,
-    PushR3,
-    PopR0,
-    PopR1,
-    PopR2,
-    PopR3,
-    IncR0,
-    IncR1,
-    IncR2,
-    IncR3,
-    DecR0,
-    DecR1,
-    DecR2,
-    DecR3,
-}
-
-#[derive(Debug, PartialEq, Eq)]
-struct OneRegisterOperand {
-    reg: u8,
-}
-
-#[derive(Debug, PartialEq, Eq)]
-struct TwoRegisterOperand {
-    reg0: u8,
-    reg1: u8,
-}
-
-#[derive(Debug, PartialEq, Eq)]
-struct BitPattern {
-    pattern: u8,
-}
-
-// take the high nibble of the instruction and decode the opcode
-fn decode_opcode(instruction: u8) -> OpCode {
+// decode and execute a byte instruction
+// we do this with simple direct decoding
+fn execute<E: Executor>(executor: &mut E, instruction: u8) {
+    // first decode the opcode
     let opcode = instruction >> 4;
-
     match opcode {
-        0 => OpCode::Special0,
-        1 => OpCode::Special1,
-        2 => OpCode::Store,
-        3 => OpCode::Load,
-        4 => OpCode::Add,
-        5 => OpCode::Sub,
-        6 => OpCode::Mul,
-        7 => OpCode::Div,
-        8 => OpCode::Eq,
-        9 => OpCode::Gt,
-        10 => OpCode::And,
-        11 => OpCode::Or,
-        12 => OpCode::Xor,
-        13 => OpCode::Unknown0,
-        14 => OpCode::Unknown1,
-        15 => OpCode::Pattern,
+        0 => execute_special0(executor, instruction),
+        1 => execute_special1(executor, instruction),
+        opcode @ 2..=14 => {
+            // the operand is the low nibble, consisting of two two-bit registers
+            let r0 = instruction & 0b0000_1100 >> 2;
+            let r1 = instruction & 0b0000_0011;
+            match opcode {
+                2 => executor.store(r0, r1),
+                3 => executor.load(r0, r1),
+                4 => executor.add(r0, r1),
+                5 => executor.sub(r0, r1),
+                6 => executor.mul(r0, r1),
+                7 => executor.div(r0, r1),
+                8 => executor.eq(r0, r1),
+                9 => executor.gt(r0, r1),
+                10 => executor.and(r0, r1),
+                11 => executor.or(r0, r1),
+                12 => executor.xor(r0, r1),
+                13 => executor.unknown0(r0, r1),
+                14 => executor.unknown1(r0, r1),
+                _ => unreachable!(),
+            }
+        }
+        15 => {
+            // pattern is a no-op
+        }
         _ => unreachable!(),
     }
 }
+
+fn execute_special0<E: Executor>(executor: &mut E, instruction: u8) {
+    match instruction {
+        0 => {
+            // block is a no-op
+        }
+        1 => {
+            executor.call();
+        }
+        2 => {
+            executor.return_();
+        }
+        3 => {
+            executor.value();
+        }
+        4..=7 => {
+            executor.if_(register_operand(instruction));
+        }
+        8..=11 => {
+            executor.repeat(register_operand(instruction));
+        }
+        12..=15 => {
+            executor.not(register_operand(instruction));
+        }
+        _ => unreachable!(),
+    }
+}
+
+fn execute_special1<E: Executor>(executor: &mut E, instruction: u8) {
+    match instruction {
+        0..=3 => {
+            executor.push(register_operand(instruction));
+        }
+        4..=7 => {
+            executor.pop(register_operand(instruction));
+        }
+        8..=11 => {
+            executor.inc(register_operand(instruction));
+        }
+        12..=15 => {
+            executor.dec(register_operand(instruction));
+        }
+        _ => unreachable!(),
+    }
+}
+
+fn register_operand(instruction: u8) -> u8 {
+    instruction & 0b0000_0011
+}
+
+struct Processor {}
+
+impl Processor {}
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    // test decode opcode function
-    #[test]
-    fn test_decode_opcode_special0() {
-        let instruction = 0b0000_0000;
-        let opcode = decode_opcode(instruction);
-        assert_eq!(opcode, OpCode::Special0);
+    struct TestExecutor {
+        trace: Vec<String>,
+    }
+
+    impl TestExecutor {
+        fn new() -> TestExecutor {
+            TestExecutor { trace: Vec::new() }
+        }
+    }
+
+    impl Executor for TestExecutor {
+        fn call(&mut self) {
+            self.trace.push("call".to_string());
+        }
+        fn return_(&mut self) {
+            self.trace.push("return".to_string());
+        }
+        fn value(&mut self) {
+            self.trace.push("value".to_string());
+        }
+        fn if_(&mut self, r0: u8) {
+            self.trace.push(format!("if {}", r0));
+        }
+        fn repeat(&mut self, r0: u8) {
+            self.trace.push(format!("repeat {}", r0));
+        }
+        fn not(&mut self, r0: u8) {
+            self.trace.push(format!("not {}", r0));
+        }
+        fn push(&mut self, r0: u8) {
+            self.trace.push(format!("push {}", r0));
+        }
+        fn pop(&mut self, r0: u8) {
+            self.trace.push(format!("pop {}", r0));
+        }
+        fn inc(&mut self, r0: u8) {
+            self.trace.push(format!("inc {}", r0));
+        }
+        fn dec(&mut self, r0: u8) {
+            self.trace.push(format!("dec {}", r0));
+        }
+        fn store(&mut self, r0: u8, r1: u8) {
+            self.trace.push(format!("store {} {}", r0, r1));
+        }
+        fn load(&mut self, r0: u8, r1: u8) {
+            self.trace.push(format!("load {} {}", r0, r1));
+        }
+        fn add(&mut self, r0: u8, r1: u8) {
+            self.trace.push(format!("add {} {}", r0, r1));
+        }
+        fn sub(&mut self, r0: u8, r1: u8) {
+            self.trace.push(format!("sub {} {}", r0, r1));
+        }
+        fn mul(&mut self, r0: u8, r1: u8) {
+            self.trace.push(format!("mul {} {}", r0, r1));
+        }
+        fn div(&mut self, r0: u8, r1: u8) {
+            self.trace.push(format!("div {} {}", r0, r1));
+        }
+        fn eq(&mut self, r0: u8, r1: u8) {
+            self.trace.push(format!("eq {} {}", r0, r1));
+        }
+        fn gt(&mut self, r0: u8, r1: u8) {
+            self.trace.push(format!("gt {} {}", r0, r1));
+        }
+        fn and(&mut self, r0: u8, r1: u8) {
+            self.trace.push(format!("and {} {}", r0, r1));
+        }
+        fn or(&mut self, r0: u8, r1: u8) {
+            self.trace.push(format!("or {} {}", r0, r1));
+        }
+        fn xor(&mut self, r0: u8, r1: u8) {
+            self.trace.push(format!("xor {} {}", r0, r1));
+        }
+        fn unknown0(&mut self, r0: u8, r1: u8) {
+            self.trace.push(format!("unknown0 {} {}", r0, r1));
+        }
+        fn unknown1(&mut self, r0: u8, r1: u8) {
+            self.trace.push(format!("unknown1 {} {}", r0, r1));
+        }
     }
 
     #[test]
-    fn test_decode_opcode_special1() {
-        let instruction = 0b0001_0000;
-        let opcode = decode_opcode(instruction);
-        assert_eq!(opcode, OpCode::Special1);
+    fn test_execute_call() {
+        let mut executor = TestExecutor::new();
+        execute(&mut executor, 0b0000_0001);
+        assert_eq!(executor.trace, vec!["call".to_string()]);
     }
 
     #[test]
-    fn test_decode_opcode_store() {
-        let instruction = 0b0010_0000;
-        let opcode = decode_opcode(instruction);
-        assert_eq!(opcode, OpCode::Store);
+    fn test_execute_if_r0() {
+        let mut executor = TestExecutor::new();
+        execute(&mut executor, 0b0000_0100);
+        assert_eq!(executor.trace, vec!["if 0".to_string()]);
     }
 
     #[test]
-    fn test_decode_opcode_load() {
-        let instruction = 0b0011_0000;
-        let opcode = decode_opcode(instruction);
-        assert_eq!(opcode, OpCode::Load);
+    fn test_execute_if_r1() {
+        let mut executor = TestExecutor::new();
+        execute(&mut executor, 0b0000_0101);
+        assert_eq!(executor.trace, vec!["if 1".to_string()]);
+    }
+
+    #[test]
+    fn test_execute_if_r2() {
+        let mut executor = TestExecutor::new();
+        execute(&mut executor, 0b0000_0110);
+        assert_eq!(executor.trace, vec!["if 2".to_string()]);
+    }
+
+    #[test]
+    fn test_execute_if_r3() {
+        let mut executor = TestExecutor::new();
+        execute(&mut executor, 0b0000_0111);
+        assert_eq!(executor.trace, vec!["if 3".to_string()]);
     }
 }
