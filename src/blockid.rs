eb122db4a989c37ad011fff4afb89b57c7d46998
Martijn Faassen <faassen@startifact.com>
2024-08-05 17:35:16 +0200

    It shouldn't be possible to be in a block and be byte 0, as split happens before.

--- src/blockid.rs
+++ src/blockid.rs
@@ -24,16 +24,10 @@ impl BlockIdentifier {
             return (0, index);
         }
         let byte = data[index];
-        if byte == 0 {
-            return (0, index);
-        }
         if is_pattern_byte(byte) {
             let high_nibble = byte & 0b0000_1111;
             if index < data.len() - 1 {
                 let next_byte = data[index + 1];
-                if next_byte == 0 {
-                    return (high_nibble << 4, index + 1);
-                }
                 // combine it with the current byte as the low nibble
                 let low_nibble = next_byte & 0b0000_1111;
                 return (high_nibble << 4 | low_nibble, index + 2);
@@ -66,20 +60,12 @@ impl BlockIdentifier {
             if index > 0 {
                 index -= 1;
                 let byte = data[index];
-                let high_nibble = if byte == 0 {
-                    index += 1;
-                    0
-                } else {
-                    byte & 0b0000_1111
-                };
+                let high_nibble = byte & 0b0000_1111;
                 return (high_nibble << 4 | low_nibble, index);
             } else {
                 // high nibble is considered 0000
                 return (low_nibble, index);
             }
-        }
-        if byte == 0 {
-            return (0, index + 1);
         }
         // we have another byte before the index that is a pattern byte
         if index > 0 {
@@ -123,26 +109,6 @@ mod tests {
     }
 
     #[test]
-    fn test_decode_byte_backward_at_start_of_block() {
-        let data = [0b0000_0000, 0b0000_0010, 0b0000_0100, 0b0000_1000];
-        let index = 1;
-        let (byte, index) = BlockIdentifier::decode_byte_backward(&data, index);
-        // at start of block we also read 0 and don't move index
-        assert_eq!(byte, 0b0000_0000);
-        assert_eq!(index, 1);
-    }
-
-    #[test]
-    fn test_decode_byte_backward_on_start_of_block() {
-        // shouldn't be possible to reach this state, but let's test it anyway
-        let data = [0b0000_0001, 0b0000_0000, 0b0000_0100, 0b0000_1000];
-        let index = 1;
-        let (byte, index) = BlockIdentifier::decode_byte_backward(&data, index);
-        assert_eq!(byte, 0b0000_0001);
-        assert_eq!(index, 0);
-    }
-
-    #[test]
     fn test_decode_byte_backward_with_full_pattern_byte() {
         let data = [0b1111_0001, 0b1111_0010];
         let index = data.len();
@@ -158,15 +124,6 @@ mod tests {
         let (byte, index) = BlockIdentifier::decode_byte_backward(&data, index);
         assert_eq!(byte, 0b0000_0010);
         assert_eq!(index, 0);
-    }
-
-    #[test]
-    fn test_decode_byte_backward_with_pattern_byte_at_start_of_block() {
-        let data = [0b0000_0000, 0b1111_0010];
-        let index = data.len();
-        let (byte, index) = BlockIdentifier::decode_byte_backward(&data, index);
-        assert_eq!(byte, 0b0000_0010);
-        assert_eq!(index, 1);
     }
 
     #[test]
@@ -206,15 +163,6 @@ mod tests {
     }
 
     #[test]
-    fn test_decode_byte_forward_at_end_of_block() {
-        let data = [0b0000_0001, 0b0000_0010, 0b0000_0100, 0b0000_0000];
-        let index = data.len() - 1;
-        let (byte, index) = BlockIdentifier::decode_byte_forward(&data, index);
-        assert_eq!(byte, 0b0000_0000);
-        assert_eq!(index, data.len() - 1);
-    }
-
-    #[test]
     fn test_decode_byte_forward_with_full_pattern_byte() {
         let data = [0b1111_0001, 0b1111_0010];
         let index = 0;
@@ -226,15 +174,6 @@ mod tests {
     #[test]
     fn test_decode_byte_forward_with_pattern_byte_at_end_of_array() {
         let data = [0b1111_0001];
-        let index = 0;
-        let (byte, index) = BlockIdentifier::decode_byte_forward(&data, index);
-        assert_eq!(byte, 0b0001_0000);
-        assert_eq!(index, 1);
-    }
-
-    #[test]
-    fn test_decode_byte_forward_with_pattern_byte_at_end_of_block() {
-        let data = [0b1111_0001, 0b0000_0000];
         let index = 0;
         let (byte, index) = BlockIdentifier::decode_byte_forward(&data, index);
         assert_eq!(byte, 0b0001_0000);
